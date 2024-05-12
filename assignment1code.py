d4603ca5d7ab16ebbe4c61ef1e635843aaf690bd
Laxmi Paudel <meluxmeme@gmail.com>
2024-05-11 19:00:12 -0600

    change on code

--- assignment1code.py
+++ assignment1code.py
@@ -2,9 +2,10 @@
 # Assignment 1 Starter Code
 # Add your name here!
 
-studentName = "Sarah Shah"  # Change the contents of this variable to your name (leave the quotation marks).
-
+studentName = "Laxmi Paudel"  # Change the contents of this variable to your name (leave the quotation marks).
+studentSkill = "Baking"
 print("Hello, World!")
 print("My name is " + studentName + " and I am a student in ENSF 692.")
 
 # print your best skill here
+print("I am good at " + studentSkill)
